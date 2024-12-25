4726854a9c8425b0364b532f87a2e42c762c5f31
yusnivtr <huuvinhkelvin@gmail.com>
2024-12-25 11:02:10 +0700

    Remove commented-out camera input code from streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,4 @@
 import streamlit as st
-# enable = st.checkbox('Use camera?')
-# image = st.camera_input("Take a picture",disabled = not enable)
-# if image:
-#     st.image(image)
-
-
 from camera_input_live import camera_input_live
 
 image = camera_input_live()
