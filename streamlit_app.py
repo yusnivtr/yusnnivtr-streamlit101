bbacb401d98902d10dd679feab99cdfb52f89b60
yusnivtr <huuvinhkelvin@gmail.com>
2024-12-25 22:14:26 +0700

    Change layout to centered in Streamlit configuration and add CSS to hide menu and footer

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,15 @@
 import streamlit as st
 
-st.set_page_config(page_title="Image Retrieval Program", page_icon=":shark:", layout="wide",initial_sidebar_state='auto')
+st.set_page_config(page_title="Image Retrieval Program", page_icon=":shark:", layout="centered", initial_sidebar_state='auto')
 
-# # hide the part of the code, as this is just for adding some custom CSS styling but not a part of the main idea 
-# hide_streamlit_style = """
-# 	<style>
-#   #MainMenu {visibility: hidden;}
-# 	footer {visibility: hidden;}
-#   </style>
-# """
-# st.markdown(hide_streamlit_style, unsafe_allow_html=True) # hide the CSS code from the screen as they are embedded in markdown text. Also, allow streamlit to unsafely process as HTML
+# hide the part of the code, as this is just for adding some custom CSS styling but not a part of the main idea 
+hide_streamlit_style = """
+	<style>
+  #MainMenu {visibility: hidden;}
+	footer {visibility: hidden;}
+  </style>
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) # hide the CSS code from the screen as they are embedded in markdown text. Also, allow streamlit to unsafely process as HTML
 
 st.title("Image Retrieval Program")
 st.info("Hehe")
