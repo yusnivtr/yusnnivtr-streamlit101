1ea7737fa713e06072a8dfc94e0b7f3887208a0f
Trần Lê Hữu Vinh <131902948+yusnivtr@users.noreply.github.com>
2024-12-25 10:19:28 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ st.title("🎈 Image Retrieve Model")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
-image = st.camera_input()
+image = st.camera_input("Take a picture")
 if image:
     st.image(image)
 
