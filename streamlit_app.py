6114e912f6e2097a9c2bb435c236713573a88f2d
yusnivtr <huuvinhkelvin@gmail.com>
2024-12-25 11:31:22 +0700

    Refactor image feedback in Streamlit app to display a message before showing the captured image

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,14 +2,14 @@ import streamlit as st
 
 st.set_page_config(page_title="Image Retrieval Program", page_icon=":shark:", layout="wide",initial_sidebar_state='auto')
 
-# hide the part of the code, as this is just for adding some custom CSS styling but not a part of the main idea 
-hide_streamlit_style = """
-	<style>
-  #MainMenu {visibility: hidden;}
-	footer {visibility: hidden;}
-  </style>
-"""
-st.markdown(hide_streamlit_style, unsafe_allow_html=True) # hide the CSS code from the screen as they are embedded in markdown text. Also, allow streamlit to unsafely process as HTML
+# # hide the part of the code, as this is just for adding some custom CSS styling but not a part of the main idea 
+# hide_streamlit_style = """
+# 	<style>
+#   #MainMenu {visibility: hidden;}
+# 	footer {visibility: hidden;}
+#   </style>
+# """
+# st.markdown(hide_streamlit_style, unsafe_allow_html=True) # hide the CSS code from the screen as they are embedded in markdown text. Also, allow streamlit to unsafely process as HTML
 
 st.header("Starting Image Retrieval Program")
 
@@ -20,7 +20,8 @@ with st.expander("Upload Image"):
         enable = st.checkbox("Enable for camera")
         image = st.camera_input('Cheer up! Smile for the camera',disabled=not enable)
         if image:
-            st.image(image)
+          st.write("You look great!")
+          st.image(image)
     if col2.button('Upload a photo',use_container_width=True):
         st.write("Upload a photo")
         image = st.file_uploader("Upload an image", type=['jpg','jpeg','png'],use_container_width=True)
