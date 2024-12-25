873b67254d171f08f52e70c7a144711ea416d711
yusnivtr <huuvinhkelvin@gmail.com>
2024-12-25 10:35:54 +0700

    Add instructions for using the camera feature in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 st.write("This is a simple example to show how to build an image retrieval model using Streamlit.")
+st.write("You can use the camera to take a picture and then retrieve the most similar images from the dataset.")
 enable = st.checkbox('Use camera?')
 image = st.camera_input("Take a picture",disabled = not enable)
 if image:
