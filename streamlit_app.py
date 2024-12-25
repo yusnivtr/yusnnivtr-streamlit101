afebebb220be3a281192a366728b22a12b33eb35
yusnivtr <huuvinhkelvin@gmail.com>
2024-12-26 00:09:42 +0700

    Hide main menu and footer in Streamlit app for a cleaner interface

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,8 @@ page_bg_img = """
   background-image: url("https://images.unsplash.com/photo-1575108921207-e6fca0c7dcf5?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
   background-size: cover;
 }
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
 <style>
 
 """
