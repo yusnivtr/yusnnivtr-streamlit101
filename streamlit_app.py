fb9a3b0b54158836169e514fdb4a64bc6e21cde1
yusnivtr <huuvinhkelvin@gmail.com>
2024-12-25 10:26:38 +0700

    Add introductory text to streamlit_app.py for image retrieval model

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,12 @@ st.title("🎈 Image Retrieve Model")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.write("This is a simple example to show how to build an image retrieval model using Streamlit.")
 enable = st.checkbox('Use camera?')
 image = st.camera_input("Take a picture",disabled = not enable)
 if image:
     st.write("Hehe")
     st.image(image)
+    
+
 
