da51d635ef12a7e9f05c423e2f8df0a641038bd1
yusnivtr <huuvinhkelvin@gmail.com>
2024-12-25 11:00:51 +0700

    Add OpenCV to requirements and implement live camera input in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,19 @@
 import streamlit as st
+import cv2
 
 st.title("🎈 Image Retrieve Model")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
-st.write("This is a simple example to show how to build an image retrieval model using Streamlit.")
-st.write("You can use the camera to take a picture and then retrieve the most similar images from the dataset.")
-enable = st.checkbox('Use camera?')
-image = st.camera_input("Take a picture",disabled = not enable)
+# enable = st.checkbox('Use camera?')
+# image = st.camera_input("Take a picture",disabled = not enable)
+# if image:
+#     st.image(image)
+
+
+from camera_input_live import camera_input_live
+
+image = camera_input_live()
+
 if image:
-    st.write("Hehe")
-    st.image(image)
+  st.image(image)
     
 
 
